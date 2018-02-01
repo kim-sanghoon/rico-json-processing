87627546aa870bde22f8a051688650b753fd73c2
Seiker <seiker@unist.ac.kr>
2018-02-01 09:56:10 +0900

    compare_org_draw improved.

--- compare_org_draw.py
+++ compare_org_draw.py
@@ -6,8 +6,22 @@ from multiprocessing.dummy import Pool
 def work(FILE_NAME):
 # for FILE_NAME in FILE_LIST:
     canvas = Image.new("RGBA", (2880, 2560), 'white')
-    origin = Image.open(os.path.join(os.path.curdir, 'original_picture', FILE_NAME.split('.')[0] + '.jpg'))
-    layout = Image.open(os.path.join(os.path.curdir, 'layout', FILE_NAME + '_out.png'))
+    try:
+        origin = Image.open(os.path.join(os.path.curdir, 'original_picture', FILE_NAME.split('.')[0] + '.jpg'))
+    except:
+        err = open('comp_err_log.txt', 'a')
+        err.write(FILE_NAME + ": Error occurred while opening original pic.")
+        err.close()
+        origin = Image.new("RGBA", (1440, 2560), "black")
+        pass
+    try:
+        layout = Image.open(os.path.join(os.path.curdir, 'layout', FILE_NAME + '_out.png'))
+    except:
+        err = open('comp_err_log.txt', 'a')
+        err.write(FILE_NAME + ": Error occurred while opening layout pic.")
+        err.close()
+        layout = Image.new("RGBA", (1440, 2560), "black")
+        pass
     # origin = Image.open('original_picture/' + FILE_NAME.split('.')[0] + '.jpg')
     # layout = Image.open('layout/' + FILE_NAME + '_out.png')
 
