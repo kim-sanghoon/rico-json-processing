990792616282e2a8b27fb23cff48e06bd0558326
Sanghoon Kim(Seiker) <seiker@unist.ac.kr>
2018-02-01 10:56:29 +0900

    Image compression for comparison images.

---
+++ compress.py
@@ -0,0 +1,30 @@
+from multiprocessing.dummy import Pool
+from PIL import Image
+from settings import *
+import os
+
+
+def work(file_name):
+    try:
+        file = open(os.path.join(os.path.curdir, 'comparison', file_name + '_comp.png'))
+    except FileNotFoundError:
+        print("File not found: " + file_name + "_comp.png")
+        return
+
+    compressed_screen_size = tuple([int(length / compress_ratio) for length in SCREEN_SIZE])
+    origin = Image.open(file)
+    new = origin.resize(compressed_screen_size, Image.ANTIALIAS)
+    new.save(os.path.join(os.path.curdir, 'compressed_comp', file_name + '_' + str(compress_ratio) + 'compressed.png'), 'PNG')
+    print(os.path.join(os.path.curdir, 'compressed_comp', file_name + '_' + str(compress_ratio) + 'compressed.png'))
+
+
+try:
+    compress_ratio = float(input("Please input compression ratio: "))
+except:
+    print("Wrong input. Default value(2.0) will be used.")
+    compress_ratio = 2.0
+
+threads = Pool(40)
+threads.map(work, FILE_LIST)
+threads.close()
+threads.join()
